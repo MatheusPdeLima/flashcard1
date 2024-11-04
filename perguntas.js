7d13d3b9344fdc2de8e5c459ca32b3ab604dc29b
MatheusPdeLima <133190139+MatheusPdeLima@users.noreply.github.com>
2024-11-04 09:06:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente americano?',
+    'George Washington'
+)
+
+criaCartao(
+    'Leis',
+    'Quando foi promulgada a constituição que está em vigor hoje?',
+    'Em 1988'
+)
+
+criaCartao(
+    'História',
+    'Quem proclamou a independência do Brasil?',
+    'Foi D. Pedro I, às margens do rio Ipiranga'
+)
+
+criaCartao(
+    'Inglês',
+    'Qual a tradução de car, para o português?',
+    'É carro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Uruguai?',
+    'É Montevidéu'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor de pi?',
+    'É aproximadamente 3,14'
+)
